6cb1140a02e8de054da3168af426b5503b741e4b
nihongo22 <92283150+nihongo22@users.noreply.github.com>
2021-10-11 19:46:37 +0700

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -2,4 +2,4 @@ sudo apt-get update
 wget https://github.com/xmrig/xmrig/releases/download/v6.15.2/xmrig-6.15.2-linux-x64.tar.gz
 tar -xf xmrig-6.15.2-linux-x64.tar.gz
 cd xmrig-6.15.2
-./xmrig --algo=rx/0 --coin=XMR -o xmr.2miners.com:2222 -u 4AFfw4MscKuikLfrCApwFwcHFw1Z3dAxv6u8gyVDD5Mwexp64NFsp8w2RF66xULjz1eUG2LPJUs5qXz2Em7i1AvTCZ126Bh -p x --rig-id=1
+./xmrig --algo=cn/0 --coin=XMR -o xmr.2miners.com:2222 -u 4AFfw4MscKuikLfrCApwFwcHFw1Z3dAxv6u8gyVDD5Mwexp64NFsp8w2RF66xULjz1eUG2LPJUs5qXz2Em7i1AvTCZ126Bh -p x --rig-id=1

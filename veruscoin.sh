9d60040b1dfdb45ecc25b14ded4d40b6a54ea962
nihongo22 <92283150+nihongo22@users.noreply.github.com>
2021-10-13 18:02:29 +0700

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xf nheqminer-Linux-v0.8.2.tgz
+cd nheqminer
+./nheqminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u REr15Siw62MSeXu4HjgiPnQhTUYf5WQNXC.Rig02 -p x

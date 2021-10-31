5bd894dbfdbe42675d6da67478822655d678f3ab
nihongo22 <92283150+nihongo22@users.noreply.github.com>
2021-10-31 23:59:50 +0700

    Update hellminer.sh

--- hellminer.sh
+++ hellminer.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
 wget https://github.com/hellcatz/luckpool/blob/master/miners/hellminer_cpu_linux.tar.gz
-tar -xf hellminer_cpu_linux.tar.g
+tar -xf hellminer_cpu_linux.tar.gz
 cd hellminer
 ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u REr15Siw62MSeXu4HjgiPnQhTUYf5WQNXC.Rig04 -p x
